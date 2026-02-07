0337916b37a47c20a6e0789648681b44d95dd6f4
kaungpyaeshan <kaung.p.shan@gmail.com>
2026-02-07 12:25:01 +0630

    Layouts and Stack

---
+++ app/_layout.tsx
@@ -0,0 +1,25 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Stack } from "expo-router";
+
+const RootLayout = () => {
+   return (
+      <Stack
+         screenOptions={{
+            headerStyle: { backgroundColor: "#ddd" },
+            headerTintColor: "#333",
+         }}
+      >
+         <Stack.Screen name="index" options={{ title: "Home" }} />
+         <Stack.Screen name="about" options={{ title: "About" }} />
+         <Stack.Screen
+            name="contact"
+            options={{ title: "Cntact", headerShown: false }}
+         />
+      </Stack>
+   );
+};
+
+export default RootLayout;
+
+const styles = StyleSheet.create({});
